5db2a6f9d10b211776fe0f5dd16ab46c5652bf9b
ErnestoArgentina <ernestoargentina@gmail.com>
2021-04-19 13:38:28 +0200

    Create AuthController.ts

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,40 @@
+import User from 'App/Models/User'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+
+  /**
+   * // Registar o usuario
+   */
+  public async register ({ request }: HttpContextContract) {
+    // return request
+    /**
+     * Validate user details
+     */
+    const validationSchema = schema.create({
+      email: schema.string({ trim: true }, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email' }),
+      ]),
+      password: schema.string({ trim: true }, [
+        rules.confirmed(),
+      ]),
+      name: schema.string.optional()
+    })
+
+    const userDetails = await request.validate({
+      schema: validationSchema,
+    })
+
+    /**
+     * Create a new user
+     */
+    const user = new User()
+    user.email = userDetails.email
+    user.password = userDetails.password
+    await user.save()
+
+    return 'Your account has been created'
+  }
+}
