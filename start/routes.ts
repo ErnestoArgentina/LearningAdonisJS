402307cc5e5b783311bae22d986d86460e9bc86c
ErnestoArgentina <ernestoargentina@gmail.com>
2021-04-19 13:42:54 +0200

    Create routes.ts

---
+++ start/routes.ts
@@ -0,0 +1,41 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes/index.ts` as follows
+|
+| import './cart'
+| import './customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy
+    ? response.ok(report)
+    : response.badRequest(report)
+})
+
+Route.on('/').render('welcome')
+Route.on('/auth').render('auth')
+
+// User
+Route.get('users', 'UsersController.index')
+Route.post('users', 'UsersController.store')
+// Auth
+Route.on('/register').render('auth/register')
+Route.post('/register', 'AuthController.register')
